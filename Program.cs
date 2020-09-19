23ef585923add941994c4d00481e1fa3f985127c
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-19 18:34:12 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace memeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string memeToNervoso;
+
+           //<---inicio(digite)--->//
+           Console.WriteLine("usuario, o que voce pensa sobre mim?");
+           memeToNervoso = Console.ReadLine();
+           Console.Write( "\n" );
+           //---fim(digite)--->//
+
+           //<---inicio(memeToNervoso)--->//
+           Console.WriteLine(" -e sabe o que eu penso de voce?");
+           Console.WriteLine($"{memeToNervoso.Remove(10)} ... não, pera .\nTo nervoso");
+           //<---fim(memeToNervoso)--->//
+
+           //<---inicio(sair)--->//
+           Console.WriteLine("pressione enter para finalizar");
+           Console.ReadKey();
+           //<---fim(sair)--->//
+        }  
+    }
+}
